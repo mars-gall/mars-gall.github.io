c97b2b7c206798d1996a7c0617a8862d2c49b9fb
Alex Wilhelm <97398812+awilh37@users.noreply.github.com>
2026-05-05 11:58:18 +0000

    capitalize player in target declaration during enemy spawn

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function animate() {
     if (gameTime - lastSpawnTime >= effectiveSpawnRate && validSpawn) {
       const enemy = new Enemy({
         position: spawnPos,
-        target: player,
+        target: Player,
       });
       enemies.push(enemy);
       lastSpawnTime = gameTime;
